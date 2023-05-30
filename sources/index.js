75caa3110b3df8979430e1ce74f3bf2c56b8b8d1
Kidd254 <lorenkioko@gmail.com>
2023-05-30 23:15:55 +0300

    add functinalities

--- sources/index.js
+++ sources/index.js
@@ -76,67 +76,16 @@ class Books {
   }
 }
 
-if (localStorage.getItem('library')) {
-  /* Reasign the books array to the localStorage library */
-  books = JSON.parse(localStorage.getItem('library'));
-}
 
-/* Constructor */
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
+/* A instance of the class that displays the added books */
+const myBooks = new Books();
+myBooks.displayBooks();
 
-/* Function that creates a book */
-function createBook(book) {
-  /* Create elements */
-  const bookContainer = document.createElement('div');
-  const bookDetails = document.createElement('div');
-  const pTitle = document.createElement('h3');
-  const pAuthor = document.createElement('p');
-  const removeButton = document.createElement('button');
-
-  /* Add Classes and properties */
-  bookContainer.classList.add('book');
-  bookDetails.classList.add('book-details');
-  removeButton.tabIndex = books.indexOf(book);
-  removeButton.name = book.title;
-
-  /* Add text */
-  pTitle.textContent = book.title;
-  pAuthor.textContent = book.author;
-  removeButton.textContent = 'Remove';
-
-  /* Append elements */
-  bookDetails.appendChild(pTitle);
-  bookDetails.appendChild(pAuthor);
-  bookContainer.appendChild(bookDetails);
-  bookContainer.appendChild(removeButton);
-  mainContainer.appendChild(bookContainer);
-
-  /* A remove book function */
-  function deleteBook() {
-    if (removeButton.name === book.title) {
-      bookContainer.remove();
-
-      /* Split and join the array */
-      const index = removeButton.tabIndex;
-      books = [...books.slice(0, index), ...books.slice(index + 1)];
-      localStorage.setItem('library', JSON.stringify(books));
-    }
-  }
-
-  /* Add a click listener to the remove button */
-  removeButton.addEventListener('click', deleteBook);
-}
-
-books.forEach((book) => {
-  createBook(book);
-});
-
+/* A function that is called when the user clicks the submit button */
 function addBook(event) {
   /* Create a new book */
-  const book = new Book(titleInput.value, authorInput.value);
+  const book = new Books();
+  book.info(titleInput.value, authorInput.value);
 
   /* If the inputs has no values then form is not submmited */
   if (!titleInput.validity.valueMissing && !authorInput.validity.valueMissing) {
@@ -145,12 +94,15 @@ function addBook(event) {
     return;
   }
 
-  /* Copy the library key to the books array */
-  books.push(book);
-  localStorage.setItem('library', JSON.stringify(books));
+  /* Save the book in the array and the local storage */
+  book.storage();
 
+  /* Resets the form */
   form.reset();
-  createBook(book);
+
+  /* Creates the new book element */
+  book.createBook(book);
 }
 
 submitButton.addEventListener('click', addBook);
+

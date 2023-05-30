9658eb238415fa144bfce308fee0a622300a337d
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-30 15:05:43 -0500

    Added methods to the class

--- sources/index.js
+++ sources/index.js
@@ -18,6 +18,62 @@ class Books {
     this.title = title;
     this.author = author;
   }
+
+  /* A method to save the info in the local storage */
+  storage() {
+    this.books.push({ title: `${this.title}`, author: `${this.author}` });
+    localStorage.setItem('library', JSON.stringify(this.books));
+  }
+
+   /* Function that creates a book */
+   createBook(book) {
+    /* Create elements */
+    const bookContainer = document.createElement('div');
+    const bookDetails = document.createElement('div');
+    const pTitle = document.createElement('h3');
+    const pAuthor = document.createElement('p');
+    const removeButton = document.createElement('button');
+
+    /* Add Classes and properties */
+    bookContainer.classList.add('book');
+    bookDetails.classList.add('book-details');
+    removeButton.tabIndex = this.books.indexOf(book);
+    removeButton.name = book.title;
+
+    /* Add text */
+    pTitle.textContent = book.title;
+    pAuthor.textContent = `by ${book.author}`;
+    removeButton.textContent = 'Remove';
+
+    /* Append elements */
+    bookDetails.appendChild(pTitle);
+    bookDetails.appendChild(pAuthor);
+    bookContainer.appendChild(bookDetails);
+    bookContainer.appendChild(removeButton);
+    mainContainer.appendChild(bookContainer);
+
+    /* A remove book function */
+    const deleteBook = () => {
+      if (removeButton.name === book.title) {
+        bookContainer.remove();
+
+        /* Split and join the array */
+        const index = removeButton.tabIndex;
+        this.books = [...this.books.slice(0, index), ...this.books.slice(index + 1)];
+        localStorage.setItem('library', JSON.stringify(this.books));
+      }
+    };
+
+    /* Add a click listener to the remove button */
+    removeButton.addEventListener('click', deleteBook);
+  }
+
+  /* A method that displays all the books elements */
+  displayBooks() {
+    this.books.forEach((book) => {
+      this.createBook(book);
+    });
+  }
 }
 
 if (localStorage.getItem('library')) {

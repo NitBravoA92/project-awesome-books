13f651804888b37d205c997c0f4618a4837d79a4
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-05-31 15:56:13 -0400

    Add the basic javascript code for the navigation

---
+++ sources/menu.js
@@ -0,0 +1,11 @@
+const pDate = document.querySelector('#date');
+const list = document.querySelector('.nav-sections:nth-child(1)');
+const addNew = document.querySelector('.nav-sections:nth-child(2)');
+const contact = document.querySelector('.nav-sections:nth-child(3)');
+const booksSection = document.querySelector('#all-awesome-books');
+const formSection = document.querySelector('#add-new-book');
+const contactSection = document.querySelector('#contact');
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const date = new Date();

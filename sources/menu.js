20c893833d098cfef73e51b75c6132f5d12cfa71
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-31 11:18:52 -0500

    Added a class that changes the color depending of the section

--- sources/menu.js
+++ sources/menu.js
@@ -52,18 +52,30 @@ function showForm() {
   booksSection.classList.add('hide');
   formSection.classList.remove('hide');
   contactSection.classList.add('hide');
+
+  list.classList.remove('change-color');
+  contact.classList.remove('change-color');
+  addNew.classList.add('change-color');
 }
 
 function showContact() {
   booksSection.classList.add('hide');
   formSection.classList.add('hide');
   contactSection.classList.remove('hide');
+
+  list.classList.remove('change-color');
+  addNew.classList.remove('change-color');
+  contact.classList.add('change-color');
 }
 
 function showBooks() {
   booksSection.classList.remove('hide');
   formSection.classList.add('hide');
   contactSection.classList.add('hide');
+
+  addNew.classList.remove('change-color');
+  contact.classList.remove('change-color');
+  list.classList.add('change-color');
 }
 
 list.addEventListener('click', showBooks);
